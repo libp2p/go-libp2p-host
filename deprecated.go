025f71fca60e87314ecb186c38e59071c4ec6ea2
Raúl Kripalani <raul@protocol.ai>
2019-04-22 14:15:20 +0100

    rename moved pkg alias to core; upgrade deps.

--- deprecated.go
+++ deprecated.go
@@ -5,15 +5,15 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/protocol"
 
-	moved "github.com/libp2p/go-libp2p-core/host"
+	core "github.com/libp2p/go-libp2p-core/host"
 )
 
 // Deprecated: use github.com/libp2p/go-libp2p-core/host.Host instead.
-type Host = moved.Host
+type Host = core.Host
 
 // Deprecated: github.com/libp2p/go-libp2p-core/peer.InfoFromHost.
 func PeerInfoFromHost(h Host) *peer.Info {
-	return moved.InfoFromHost(h)
+	return core.InfoFromHost(h)
 }
 
 // Deprecated: use github.com/libp2p/go-libp2p-core/helpers.MultistreamSemverMatcher.
--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/libp2p/go-libp2p-host
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
-	github.com/libp2p/go-libp2p-core v0.0.0-20190417162149-f41b5dd7c01b
+	github.com/libp2p/go-libp2p-core v0.0.0-20190422125857-3a54eb68d178
 	golang.org/x/crypto v0.0.0-20190228161510-8dd112bcdc25 // indirect
 	golang.org/x/sys v0.0.0-20190228124157-a34e9553db1e // indirect
 )
